6ce6efb745cac5f5c9397c007dc1558e6e2aa271
Anton Ametov <antonametov88@gmail.com>
2017-10-23 15:16:12 +0300

    fixes

--- Runner/Assets/Scripts/Game/Commands/RestartLevelCommand.cs
+++ Runner/Assets/Scripts/Game/Commands/RestartLevelCommand.cs
@@ -1,15 +1,25 @@
 ﻿using Core.Binder;
 using Core.Commands;
 using Game.Model;
+using UnityEngine;
 
 namespace Game.Commands
 {
     public class RestartLevelCommand : ICommand
     {
+        private Transform _cameraTransform;
+
+        public RestartLevelCommand(Transform cameraTransform)
+        {
+            _cameraTransform = cameraTransform;
+        }
+        
         public void Execute()
         {
             var levelModel = BindManager.GetInstance<LevelSessionModel>();
             levelModel.Player.Reset(levelModel.Level.StartPosition);
+            
+            _cameraTransform.position = levelModel.Level.StartPosition;
 
             levelModel.Player.Activate(true);
             levelModel.Player.Accelerate();
--- Runner/Assets/Scripts/Game/Gui/GameView/GameView.cs
+++ Runner/Assets/Scripts/Game/Gui/GameView/GameView.cs
@@ -11,7 +11,6 @@ using Game.Model;
 using Core.Commands;
 using Game.Commands;
 using Game.Data;
-using UnityEngine.SceneManagement;
 
 namespace Game.Gui.GameView
 {
@@ -19,7 +18,6 @@ namespace Game.Gui.GameView
     {
         private float _time = 0;
         
-
         [SerializeField] private Transform _levelHolder;
 
         [SerializeField] private CameraSmoothFollow _camera;
@@ -41,11 +39,11 @@ namespace Game.Gui.GameView
             _dispatcher.AddListener(LevelEventsEnum.Finish, OnFinishLevel);
 
             _levelModel = BindManager.GetInstance<LevelSessionModel>();
-
-            _camera.CachedTransform.position = _levelModel.Player.CachedTransform.position;
+            
             _camera.target = _levelModel.Player.CachedTransform;
 
-            ScheduleUpdate(1, false);
+            _restartLevelCommand = new RestartLevelCommand(_camera.CachedTransform);
+            _restartLevelCommand.Execute();
         }
 
         protected override void Update()
@@ -53,46 +51,17 @@ namespace Game.Gui.GameView
             base.Update();
 
             _time += Time.deltaTime;
-            
         }
 
         private void OnFinishLevel()
         {
             _levelModel.LevelTime = _time;
-            //ViewManager.Instance.SetView(ViewNames.LevelView, _levelModel.EpisodeId);
             
-            if (!_paused)
-            {
-                ViewManager.Instance.SetView(ViewNames.FinishView);
-
-                Time.timeScale = 0;
-
-                _paused = true;
-
-            }
-
-                //ViewManager.Instance.SetView(ViewNames.MainMenuScreen);
-                //ViewManager.Instance.SetView(ViewNames.LevelView, _levelModel.EpisodeId);
-                //ViewManager.Instance.GetLayerById(LayerNames.ThreeDLayer).RemoveCurrentView();
-                //_levelModel.LevelTime = _time;
-            }
-
-        protected override void OnScheduledUpdate()
-        {
-            base.OnScheduledUpdate();
-
-            _restartLevelCommand = new RestartLevelCommand();
-            _restartLevelCommand.Execute();
-
-            _camera.CachedTransform.position = _levelModel.Player.CachedTransform.position;
-
-            _levelModel.Player.Activate(true);
-            _levelModel.Player.Accelerate();
+            ViewManager.Instance.SetView(ViewNames.FinishView);
         }
         
         private void OnPlayerRestart()
         {
-            _restartLevelCommand = new RestartLevelCommand();
             _restartLevelCommand.Execute();
         }
 

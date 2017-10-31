433177db089d9ed0b0a98ee1bc4083a318b6a79a
DESKTOP-QJ8EFBA\Arti <ametovart@gmail.com>
2017-10-31 02:15:33 +0200

    Create Level6, ADD new player, Update all level prefab,

---
+++ Runner/Assets/Scripts/Game/Player/PlayerAnimation.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Game.Player
+{
+    public class PlayerAnimation : MonoBehaviour
+    {
+        private Animation animation;
+
+        private void Awake()
+        {
+            animation = GetComponent<Animation>();
+        }
+
+        public void DidJump()
+        {
+            animation.Play(Tags.ANIMATION_JUMP);
+            animation.PlayQueued(Tags.ANIMATION_JUMP_FALL);
+        }
+
+        public void DidLand()
+        {
+            animation.Stop(Tags.ANIMATION_JUMP_FALL);
+            animation.Stop(Tags.ANIMATION_JUMP_LAND);
+            animation.Blend(Tags.ANIMATION_JUMP_LAND, 0);
+            animation.CrossFade(Tags.ANIMATION_RUN);
+        }
+
+        public void PlayerRun()
+        {
+            animation.Play(Tags.ANIMATION_RUN);
+        }
+    }
+}

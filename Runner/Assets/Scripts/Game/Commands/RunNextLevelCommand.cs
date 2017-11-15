5c669da3185d47face2d726016d5eeb55fb72551
DESKTOP-QJ8EFBA\Arti <ametovart@gmail.com>
2017-11-15 11:43:08 +0200

    Fixed Pause Menu, Level fixed

---
+++ Runner/Assets/Scripts/Game/Commands/RunNextLevelCommand.cs
@@ -0,0 +1,20 @@
+﻿using Core.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game.Commands
+{
+    public class RunNextLevelCommand : ICommand
+    {
+
+
+        public void Execute()
+        {
+
+        }
+
+
+    }
+}
--- Runner/Assets/Scripts/Game/Gui/LevelWindow/PauseView.cs
+++ Runner/Assets/Scripts/Game/Gui/LevelWindow/PauseView.cs
@@ -1,4 +1,5 @@
-﻿using Core.Binder;
+﻿using System;
+using Core.Binder;
 using Core.ViewManager;
 using Game.Data;
 using Game.Model;
@@ -13,6 +14,8 @@ namespace Game.Gui.LevelWindow
 
         [SerializeField] private Button _backButton;
 
+        [SerializeField] private Button _MenuButton;
+
         [SerializeField] private Text _levelTime;
 
         protected override void Start()
@@ -21,9 +24,16 @@ namespace Game.Gui.LevelWindow
 
             _backButton.onClick.AddListener(OnBackClick);
 
+            _MenuButton.onClick.AddListener(OnMenuClick);
+
             _levelModel = BindManager.GetInstance<LevelSessionModel>();
             
             _levelTime.text = _levelModel.LevelTime.ToString();
+        }
+
+        private void OnMenuClick()
+        {
+            ViewManager.Instance.SetView(ViewNames.LevelView);
         }
 
         private void OnBackClick()
